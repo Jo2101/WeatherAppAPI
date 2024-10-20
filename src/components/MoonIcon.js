8e38f62fcf7fc4b7d595e6bb0669bd673f7664b2
Jo2101 <jovana@darkleap.dev>
2024-10-21 00:52:06 +0200

    nn

--- src/components/MoonIcon.js
+++ src/components/MoonIcon.js
@@ -1,10 +1,11 @@
 import data from "../data/weather.json";
 import moonLogo from "../img/moonLogo.png";
+import sunLogo from "../img/sunLogo.png";
 
 const MoonIcon = () => {
   return (
     <img
-      src={data.icon == "moonLogo" ? moonLogo : ""}
+      src={data.icon == "moonLogo" ? moonLogo : sunLogo}
       style={{
         width: "100px",
         height: "100px",
--- src/components/SunIcon.js
+++ src/components/SunIcon.js
@@ -1,10 +1,11 @@
 import data from "../data/weather.json";
 import sunLogo from "../img/sunLogo.png";
+import moonLogo from "../img/moonLogo.png";
 
 const SunIcon = () => {
   return (
     <img
-      src={data.icon == "sunLogo" ? sunLogo : ""}
+      src={data.icon == "sunLogo" ? sunLogo : moonLogo}
       style={{ width: "100px", height: "100px" }}
     />
   );
